f90042d8ef523a5bc5d02316d94eb593df40df0f
Iba 🌙 <salaam.iba@gmail.com>
2026-03-13 14:42:03 +0000

    Z-2 User package setup - entity, DTOs, repository, service and controller (#1)

--- src/main/java/com/zenith/zenith_app/user/UserController.java
+++ src/main/java/com/zenith/zenith_app/user/UserController.java
@@ -1,4 +1,28 @@
 package com.zenith.zenith_app.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public UserDTO register(@RequestBody RegisterRequest registerRequest){
+        return userService.register(registerRequest);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDTO viewProfile(@PathVariable Long userId){
+        return userService.viewProfile(userId);
+    }
+
+    @PutMapping("/{userId}")
+    public UserDTO updateProfile(@PathVariable Long userId, @RequestBody UpdateProfileRequest updateProfileRequest){
+        return userService.updateProfile(userId, updateProfileRequest);
+    }
+
 }
