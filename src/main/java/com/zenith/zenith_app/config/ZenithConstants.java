fe376956c447eceb8d4349c2b7a72a70577e0a60
minimoon3010 <salaam.iba@gmail.com>
2026-03-27 21:41:44 +0000

    Z-9 Transaction feature implementation and adjusting service class with XP logic.

--- src/main/java/com/zenith/zenith_app/config/ZenithConstants.java
+++ src/main/java/com/zenith/zenith_app/config/ZenithConstants.java
@@ -4,4 +4,6 @@ public class ZenithConstants {
     public static final int STAR_XP = 25;
     public static final int CONSTELLATION_BONUS_XP_PER_STAR = 15;
     public static final int CONSTELLATION_MIN_STARS_FOR_BONUS = 3;
+    public static final int TRANSACTION_DISMISSED_XP = 30;
+    public static final int TRANSACTION_IMPULSE_XP = 40;
 }
